b4c8557b9a3c60464c15fea9dd70857d9f523799
HanHyeJin <rumgam@naver.com>
2016-12-09 15:08:15 +0900

    코드 분석 및 주석달기

--- Steganography/SteganographyHelper.cs
+++ Steganography/SteganographyHelper.cs
@@ -11,11 +11,12 @@ namespace Steganography
             Filling_With_Zeros
         };
 
+        // 숨김원리
         public static Bitmap embedText(string text, Bitmap bmp)
         {
             State state = State.Hiding;
 
-            int charIndex = 0;
+            int charIndex = 0; 
 
             int charValue = 0;
 
@@ -25,23 +26,32 @@ namespace Steganography
 
             int R = 0, G = 0, B = 0;
 
-            for (int i = 0; i < bmp.Height; i++)
+            /* RGB 값 추출 
+             가장 일반적으로 스테가노그래피 수정은 최하위 비트(LSB)를 고친다. 
+             이 비트를 0에서 1로, 1에서 0으로 바뀌고 이 비트가 복원되면 원래의 숨겨진 메시지가 나타난다. 
+             RGB로 색을 표현할 때 24비트 색상 팔레트로 표현한다.
+             RED, GREEN, BLUE 8비트씩 각각 256가지의 레드, 그린, 블루를 표현한다.
+             LSB를 수정하였을 경우 우리 눈으로 차이점을 
+             찾아내기 매우 어렵기 때문에 데이터를 숨기기에 
+             적합한 곳으로 많이 이용된다. */
+            for (int i = 0; i < bmp.Height; i++) // 높이
             {
-                for (int j = 0; j < bmp.Width; j++)
+                for (int j = 0; j < bmp.Width; j++) //너비
                 {
-                    Color pixel = bmp.GetPixel(j, i);
+                    Color pixel = bmp.GetPixel(j, i); //그림을 좌표로 생각하고 가져옴,  해당 System.Drawing.Bitmap의 지정된 픽셀의 색을 가져옴
 
-                    R = pixel.R - pixel.R % 2;
+                    // RGB 값 추출 
+                    R = pixel.R - pixel.R % 2; 
                     G = pixel.G - pixel.G % 2;
                     B = pixel.B - pixel.B % 2;
 
-                    for (int n = 0; n < 3; n++)
+                    for (int n = 0; n < 3; n++) //rgb 하나하나씩해서 3번 반복
                     {
-                        if (pixelElementIndex % 8 == 0)
+                        if (pixelElementIndex % 8 == 0) // 8비트 끝(LSB)에 숨기므로 그 때 일때만 수행
                         {
-                            if (state == State.Filling_With_Zeros && zeros == 8)
+                            if (state == State.Filling_With_Zeros && zeros == 8) //숨기는 곳과 0으로 채우는 것이 같고 zeros가 8일때 수행
                             {
-                                if ((pixelElementIndex - 1) % 3 < 2)
+                                if ((pixelElementIndex - 1) % 3 < 2) //pixelElementIndex = 1,2,4,5,7,8 (3,6빼고 수행)  
                                 {
                                     bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
                                 }
@@ -49,19 +59,20 @@ namespace Steganography
                                 return bmp;
                             }
 
-                            if (charIndex >= text.Length)
+                            if (charIndex >= text.Length) //char 인데스가 입력 글자보다 커지면 hiding 부분을 zero로 채우기(state = State.Filling_With_Zeros)
                             {
-                                state = State.Filling_With_Zeros;
+                                state = State.Filling_With_Zeros; 
                             }
-                            else
+                            else 
                             {
                                 charValue = text[charIndex++];
                             }
                         }
 
-                        switch (pixelElementIndex % 3)
+
+                        switch (pixelElementIndex % 3) // 1,2,3,4,5,6,7,8
                         {
-                            case 0:
+                            case 0: //R 값 수정
                                 {
                                     if (state == State.Hiding)
                                     {
@@ -69,7 +80,7 @@ namespace Steganography
                                         charValue /= 2;
                                     }
                                 } break;
-                            case 1:
+                            case 1: //G 값 수정
                                 {
                                     if (state == State.Hiding)
                                     {
@@ -78,7 +89,7 @@ namespace Steganography
                                         charValue /= 2;
                                     }
                                 } break;
-                            case 2:
+                            case 2://B 값 수정
                                 {
                                     if (state == State.Hiding)
                                     {
@@ -104,6 +115,8 @@ namespace Steganography
             return bmp;
         }
 
+
+        // 추출원리
         public static string extractText(Bitmap bmp)
         {
             int colorUnitIndex = 0;
@@ -111,14 +124,14 @@ namespace Steganography
 
             string extractedText = String.Empty;
 
-            for (int i = 0; i < bmp.Height; i++)
+            for (int i = 0; i < bmp.Height; i++) //그림의 높이
             {
-                for (int j = 0; j < bmp.Width; j++)
+                for (int j = 0; j < bmp.Width; j++) // 그림의 너비
                 {
                     Color pixel = bmp.GetPixel(j, i);
                     for (int n = 0; n < 3; n++)
                     {
-                        switch (colorUnitIndex % 3)
+                        switch (colorUnitIndex % 3) // 숨김과 반대로 해줌
                         {
                             case 0:
                                 {
@@ -140,7 +153,7 @@ namespace Steganography
                         {
                             charValue = reverseBits(charValue);
 
-                            if (charValue == 0)
+                            if (charValue == 0) //끝까지 다 추출하면 추출된 글자 반환
                             {
                                 return extractedText;
                             }
@@ -155,11 +168,11 @@ namespace Steganography
             return extractedText;
         }
 
-        public static int reverseBits(int n)
+        public static int reverseBits(int n) //bit을 반대로 
         {
             int result = 0;
 
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < 8; i++) // 8bit 까지 쭉 돌려줌
             {
                 result = result * 2 + n % 2;
 
