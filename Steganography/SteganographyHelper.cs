2104e9abfa293a977e4e9d52ca86913c4be7e4dd
yu-ra kim <dbfkdlf@naver.com>
2017-12-04 10:44:36 +0900

    added comment
    Signed-off-by: yu-ra kim <dbfkdlf@naver.com>

--- Steganography/SteganographyHelper.cs
+++ Steganography/SteganographyHelper.cs
@@ -11,89 +11,90 @@ namespace Steganography
             Filling_With_Zeros
         };
 
-        public static Bitmap embedText(string text, Bitmap bmp)
+        public static Bitmap embedText(string text, Bitmap bmp)//이미지에 문자를 숨기는 함수 
         {
             State state = State.Hiding;
 
-            int charIndex = 0;
 
-            int charValue = 0;
 
-            long pixelElementIndex = 0;
+            int charIndex = 0; // 숨겨질 문자의 인덱스
+            int charValue = 0;//인티저로 변환 된 문자 값
 
-            int zeros = 0;
+            long pixelElementIndex = 0;//처리중인 픽셀의 RGB 인덱스
 
-            int R = 0, G = 0, B = 0;
+            int zeros = 0; //8비트 count
 
-            for (int i = 0; i < bmp.Height; i++)
+            int R = 0, G = 0, B = 0;//픽셀에 있는 요소들
+
+            for (int i = 0; i < bmp.Height; i++)//이미지의 height만큼 반복
             {
-                for (int j = 0; j < bmp.Width; j++)
+                for (int j = 0; j < bmp.Width; j++)//이미지의 width만큼 반복
                 {
-                    Color pixel = bmp.GetPixel(j, i);
+                    Color pixel = bmp.GetPixel(j, i);//이미지의 (height, width), 현재 처리중인 픽셀
 
-                    R = pixel.R - pixel.R % 2;
-                    G = pixel.G - pixel.G % 2;
-                    B = pixel.B - pixel.B % 2;
+                    R = pixel.R - pixel.R % 2;//2나눈 만큼 빼줘서 LSB를 0으로 맞춤, 예를들어 R이 3이었다면 2로나눈 나머지인 1을 3에서 빼줌= 00000010
+                    G = pixel.G - pixel.G % 2;//2나눈 만큼 빼줘서 LSB를 0으로 맞춤
+                    B = pixel.B - pixel.B % 2;//2나눈 만큼 빼줘서 LSB를 0으로 맞춤
 
-                    for (int n = 0; n < 3; n++)
+                    for (int n = 0; n < 3; n++)//3번반복,(R,G,B)
                     {
-                        if (pixelElementIndex % 8 == 0)
+                        if (pixelElementIndex % 8 == 0)//pixelElementIndex가 8의 배수면
                         {
-                            if (state == State.Filling_With_Zeros && zeros == 8)
+                            if (state == State.Filling_With_Zeros && zeros == 8)//0이 8개인지 확인
                             {
-                                if ((pixelElementIndex - 1) % 3 < 2)
+                                if ((pixelElementIndex - 1) % 3 < 2)//이미지의 마지막픽셀
                                 {
-                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));//컬러정해주고 
                                 }
 
-                                return bmp;
+                                return bmp;//텍스트를 다 숨기고 반환
                             }
 
-                            if (charIndex >= text.Length)
+                            if (charIndex >= text.Length)//텍스트의 길이와 비교해서 모든 문자가 숨겨졌다면
                             {
-                                state = State.Filling_With_Zeros;
+                                state = State.Filling_With_Zeros;//텍스트의 끝이므로 0추가
                             }
-                            else
+                            else//아니라면
                             {
-                                charValue = text[charIndex++];
+                                charValue = text[charIndex++];//다음문자로 이동
                             }
                         }
 
-                        switch (pixelElementIndex % 3)
+                        switch (pixelElementIndex % 3)//switch문으로 3으로 나눠서 RGB구분 
                         {
-                            case 0:
+                            case 0://레드
                                 {
                                     if (state == State.Hiding)
                                     {
-                                        R += charValue % 2;
-                                        charValue /= 2;
+                                        R += charValue % 2;//숨길 문자의 이진값을 2로나눈 나머지 추가
+                                        charValue /= 2;//숨길 문자를 2로 나눠서 맨 마지막 비트 없앰
                                     }
                                 } break;
-                            case 1:
+                            case 1://그린
                                 {
                                     if (state == State.Hiding)
                                     {
-                                        G += charValue % 2;
+                                        G += charValue % 2;//숨길 문자의 이진값을 2로나눈 나머지 추가
 
-                                        charValue /= 2;
+                                        charValue /= 2;//숨길 문자를 2로 나눠서 맨 마지막 비트 없앰
                                     }
                                 } break;
-                            case 2:
+                            case 2://블루
                                 {
                                     if (state == State.Hiding)
                                     {
-                                        B += charValue % 2;
+                                        B += charValue % 2;//숨길 문자를 2로 나눠서 맨 마지막 비트 없앰
 
-                                        charValue /= 2;
+                                        charValue /= 2;//숨길 문자를 2로 나눠서 맨 마지막 비트 없앰
                                     }
 
-                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));//설정해준 값으로 컬러 
                                 } break;
                         }
 
-                        pixelElementIndex++;
+                        pixelElementIndex++;//RGB인덱스를 1 높여줌
 
-                        if (state == State.Filling_With_Zeros)
+                        if (state == State.Filling_With_Zeros)//8이 될때까지 1을 더함
                         {
                             zeros++;
                         }
@@ -101,61 +102,61 @@ namespace Steganography
                 }
             }
 
-            return bmp;
+            return bmp;//이미지 반환
         }
 
-        public static string extractText(Bitmap bmp)
+        public static string extractText(Bitmap bmp)//텍스트를 다시 추출하는 함수
         {
-            int colorUnitIndex = 0;
+            int colorUnitIndex = 0;//픽셀의 RGB인덱스
             int charValue = 0;
 
-            string extractedText = String.Empty;
+            string extractedText = String.Empty;//이미지에서 추출한 텍스트 
 
-            for (int i = 0; i < bmp.Height; i++)
+            for (int i = 0; i < bmp.Height; i++)//아까처럼 이미지의 높이만큼 반복
             {
-                for (int j = 0; j < bmp.Width; j++)
+                for (int j = 0; j < bmp.Width; j++)//이미지의 폭만큼 반복
                 {
-                    Color pixel = bmp.GetPixel(j, i);
-                    for (int n = 0; n < 3; n++)
+                    Color pixel = bmp.GetPixel(j, i);//현재 픽셀 저장 
+                    for (int n = 0; n < 3; n++)//RGB 3번반복
                     {
-                        switch (colorUnitIndex % 3)
+                        switch (colorUnitIndex % 3)//3으로 나눠서 RGB구분
                         {
-                            case 0:
+                            case 0://레드
                                 {
-                                    charValue = charValue * 2 + pixel.R % 2;
+                                    charValue = charValue * 2 + pixel.R % 2;//팍셀의 LSB를 문자 비트에 더해줌
                                 } break;
-                            case 1:
+                            case 1://그린
                                 {
-                                    charValue = charValue * 2 + pixel.G % 2;
+                                    charValue = charValue * 2 + pixel.G % 2;//2를 곱해줘서 맨 오른쪽에 더해줄 수 있게 함 
                                 } break;
-                            case 2:
+                            case 2://블루
                                 {
-                                    charValue = charValue * 2 + pixel.B % 2;
+                                    charValue = charValue * 2 + pixel.B % 2;//마찬가지
                                 } break;
                         }
 
-                        colorUnitIndex++;
+                        colorUnitIndex++;//인덱스에 1 더해줌
 
-                        if (colorUnitIndex % 8 == 0)
+                        if (colorUnitIndex % 8 == 0)//8비트라면 
                         {
-                            charValue = reverseBits(charValue);
+                            charValue = reverseBits(charValue);//비트를 뒤집어서 저장
 
-                            if (charValue == 0)
+                            if (charValue == 0)//0이라면
                             {
-                                return extractedText;
+                                return extractedText;//리턴(0이8개면 끝)
                             }
-                            char c = (char)charValue;
+                            char c = (char)charValue;//문자를 char로 변환
 
-                            extractedText += c.ToString();
+                            extractedText += c.ToString();//문자를 결과에 추가
                         }
                     }
                 }
             }
 
-            return extractedText;
+            return extractedText;//다하면 반환
         }
 
-        public static int reverseBits(int n)
+        public static int reverseBits(int n)//비트 뒤집는 함수
         {
             int result = 0;
 
