75cccd9d2c48e7e15bdaac539d3d3d8dc6738384
choi seungdo <choiseungdo@naver.com>
2018-12-08 14:52:41 +0900

    코드 주석 추가 (assem, helper)

--- Steganography/Properties/AssemblyInfo.cs
+++ Steganography/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Steganography")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// comvisible을 false로 설정하면 이 어셈블러의 유형이 com 구성 요소에 표시되지 않는다.
+//com에서 이 어셈블리의 유형에 엑세스 해야하는 경우 comvisible 특성을 해당 유형에서 true로 설정해야한다.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+//다음 GUID는 이 프로젝트가 COM에 노출될 경우 typeelib ID에 대한 것이다.
 [assembly: Guid("9997a01d-a864-4507-8877-4ebcb52fe1bf")]
 
-// Version information for an assembly consists of the following four values:
+//어셈블리어를 위한 버전 정보는 네 가지 값으로 구성된다.
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+//주 버전
+//보조 버전
+//빌드 번호
+//개정
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+//모든 값을 지정하거나 빌드 및 수정 번호를 기본 값으로 설정할 수 있다.
+//아래와 같이 '*'를 사용한다.
+//[assemblyversion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Steganography/SteganographyHelper.cs
+++ Steganography/SteganographyHelper.cs
@@ -7,93 +7,101 @@ namespace Steganography
     {
         public enum State
         {
-            Hiding,
-            Filling_With_Zeros
+            Hiding,                     // 데이터 숨길 것이 있음
+            Filling_With_Zeros      // 데이터 숨길 것이 없음
         };
 
+        // text = data,  bmp = 숨길 이미지, 데이터 숨기는 함수임
         public static Bitmap embedText(string text, Bitmap bmp)
         {
-            State state = State.Hiding;
+            State state = State.Hiding; // 데이터 숨길 것이 있음으로 설정
 
-            int charIndex = 0;
+            int charIndex = 0;              // 숨긴 문자 수
 
-            int charValue = 0;
+            int charValue = 0;              // 숨긴 문자
 
-            long pixelElementIndex = 0;
+            long pixelElementIndex = 0;     // 카운트 인덱스
 
-            int zeros = 0;
+            int zeros = 0;  // 숨길것이 없는 상태에서 픽셀을 순회하면 1씩 증가
 
             int R = 0, G = 0, B = 0;
 
-            for (int i = 0; i < bmp.Height; i++)
+            // 이미지의 사이즈 만큼의 모든 픽셀을 이중 for문을 통해서 순회
+            for (int i = 0; i < bmp.Height; i++)            // 이미지의 높이 만큼 반복문 실행
             {
-                for (int j = 0; j < bmp.Width; j++)
+                for (int j = 0; j < bmp.Width; j++)         // 이미지의 넓이 만큼 반복문 실행
                 {
-                    Color pixel = bmp.GetPixel(j, i);
+                    Color pixel = bmp.GetPixel(j, i);       // 픽셀을 가져옴
 
+                    // pixel.R G B의 값을 이용해, 정수형 R, G, B에 새로운 값을 저장
+                    // 즉 RGB값을 2진수로 봤을 때 , 마지막 값을 무조건 0으로 맞춰줌
                     R = pixel.R - pixel.R % 2;
                     G = pixel.G - pixel.G % 2;
                     B = pixel.B - pixel.B % 2;
 
-                    for (int n = 0; n < 3; n++)
+                    // 한 픽셀의 R, G, B값을 반복문으로 확인하여 조건에 만족하면 그 값을 변경
+                    for (int n = 0; n < 3; n++)     // 한 픽셀의 R, G, B 값을 반복
                     {
-                        if (pixelElementIndex % 8 == 0)
+                        if (pixelElementIndex % 8 == 0) // 픽셀요소인덱스가 8번째 마다 이면
                         {
-                            if (state == State.Filling_With_Zeros && zeros == 8)
+                            if (state == State.Filling_With_Zeros && zeros == 8)    // 숨길것이 없는 상태 + 숨길것이 없는상태에서 8번째이면
                             {
-                                if ((pixelElementIndex - 1) % 3 < 2)
+                                if ((pixelElementIndex - 1) % 3 < 2)    // 픽셀값을 설정하고 비트맵 반환 숨김 종료
                                 {
-                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));    // 픽셀의 정보를 설정
                                 }
 
-                                return bmp;
+                                return bmp; // 위의 모든 조건이 만족하면 bmp 반환
                             }
 
-                            if (charIndex >= text.Length)
+                            if (charIndex >= text.Length)   // 현재 숨긴 문자가 숨길 문자의 길이보다 크거나 같으면
                             {
-                                state = State.Filling_With_Zeros;
+                                state = State.Filling_With_Zeros;   // 숨길것이 없는 상태로 상태 설정
                             }
-                            else
+                            else  //  아니라면 charValue에 숨길 문자를 대입
                             {
                                 charValue = text[charIndex++];
                             }
                         }
 
-                        switch (pixelElementIndex % 3)
+                        switch (pixelElementIndex % 3) // 픽셀요소인덱스를 3으로 나눈 나머지 값으로 switch문을 실행
                         {
-                            case 0:
+                            case 0: // 나머지가 0이면
                                 {
-                                    if (state == State.Hiding)
+                                    if (state == State.Hiding)  // state가 숨길것이 있는 상태이면
                                     {
-                                        R += charValue % 2;
+                                        R += charValue % 2; // R의 값을 변경
                                         charValue /= 2;
                                     }
-                                } break;
-                            case 1:
+                                }
+                                break;
+                            case 1: // 나머지가 1이면
                                 {
-                                    if (state == State.Hiding)
+                                    if (state == State.Hiding) // state가 숨길것이 있는 상태이면
                                     {
-                                        G += charValue % 2;
+                                        G += charValue % 2; // G의 값을 변경
 
                                         charValue /= 2;
                                     }
-                                } break;
+                                }
+                                break;
                             case 2:
                                 {
-                                    if (state == State.Hiding)
+                                    if (state == State.Hiding) // state가 숨길것이 있는 상태이면
                                     {
-                                        B += charValue % 2;
+                                        B += charValue % 2; // B의 값을 변경
 
                                         charValue /= 2;
                                     }
 
-                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
-                                } break;
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));    // 변경된 RGB값으로 픽셀을 설정
+                                }
+                                break;
                         }
 
-                        pixelElementIndex++;
+                        pixelElementIndex++;    // count를 해준다.
 
-                        if (state == State.Filling_With_Zeros)
+                        if (state == State.Filling_With_Zeros)  // 픽셀 반복중에 state가 숨길것이 없는 상태이면, zeros +1 해줌
                         {
                             zeros++;
                         }
@@ -104,55 +112,60 @@ namespace Steganography
             return bmp;
         }
 
-        public static string extractText(Bitmap bmp)
+        public static string extractText(Bitmap bmp)       // 데이터를 추출하는 함수
         {
             int colorUnitIndex = 0;
             int charValue = 0;
 
             string extractedText = String.Empty;
 
-            for (int i = 0; i < bmp.Height; i++)
+            for (int i = 0; i < bmp.Height; i++)    // 이미지의 높이 만큼 반복 수행
             {
-                for (int j = 0; j < bmp.Width; j++)
+                for (int j = 0; j < bmp.Width; j++) // 이미지의 너비 만큼 반복 수행
                 {
-                    Color pixel = bmp.GetPixel(j, i);
+                    Color pixel = bmp.GetPixel(j, i);   // 이미지의 픽셀을 얻어옴
+
+
                     for (int n = 0; n < 3; n++)
                     {
-                        switch (colorUnitIndex % 3)
+                        switch (colorUnitIndex % 3) // // case마다 숨김에서 한 연산의 반대
                         {
-                            case 0:
+                            case 0: // 나머지가 0이면
                                 {
-                                    charValue = charValue * 2 + pixel.R % 2;
-                                } break;
-                            case 1:
+                                    charValue = charValue * 2 + pixel.R % 2;    // R픽셀을 2로 나눈나머지를 charValue*2의 값과 더한 후 charValue에 저장
+                                }
+                                break;
+                            case 1: // 나머지가 1이면
                                 {
-                                    charValue = charValue * 2 + pixel.G % 2;
-                                } break;
-                            case 2:
+                                    charValue = charValue * 2 + pixel.G % 2;    // G픽셀을 2로 나눈나머지를 charValue*2의 값과 더한 후 charValue에 저장
+                                }
+                                break;
+                            case 2: // 나머지가 2이면
                                 {
-                                    charValue = charValue * 2 + pixel.B % 2;
-                                } break;
+                                    charValue = charValue * 2 + pixel.B % 2;    // B픽셀을 2로 나눈나머지를 charValue*2의 값과 더한 후 charValue에 저장
+                                }
+                                break;
                         }
 
-                        colorUnitIndex++;
+                        colorUnitIndex++;   //색 인덱스를 카운드 +1
 
-                        if (colorUnitIndex % 8 == 0)
+                        if (colorUnitIndex % 8 == 0)    // 색인덱스을 8로 나눈 나머지가 0이면
                         {
                             charValue = reverseBits(charValue);
 
-                            if (charValue == 0)
+                            if (charValue == 0) // charValue의 값이 0이면
                             {
-                                return extractedText;
+                                return extractedText;   // extractedText 스트링을 반환
                             }
-                            char c = (char)charValue;
+                            char c = (char)charValue;   //charValue를 캐릭터 형으로 변경 해서 문자 하나를 얻어냄
 
-                            extractedText += c.ToString();
+                            extractedText += c.ToString();  // 그 문자를 extractedText에 추가
                         }
                     }
                 }
             }
 
-            return extractedText;
+            return extractedText;   // 모든 반복문이 종료되면 extractedText를 반환
         }
 
         public static int reverseBits(int n)
